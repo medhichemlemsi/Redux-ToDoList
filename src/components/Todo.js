029afc1e75701350e4cddb155470a05aead5a897
medhichemlemsi <89579352+medhichemlemsi@users.noreply.github.com>
2021-11-16 19:36:05 +0100

    first

---
+++ src/components/Todo.js
@@ -0,0 +1,29 @@
+import { Box, Checkbox, Text,Button } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { Delete, toggleTodo } from '../redux/actions';
+
+export const Todo = ({ todo }) => {
+  const dispatch = useDispatch();
+  const [cheked, setCheked] = useState(false);
+
+  const handleCheked = () => dispatch(toggleTodo(todo.id));
+
+  useEffect(() => {
+    setCheked(todo.completed);
+  }, [todo]);
+
+  return (
+    <div>
+    <Box mb={1} bgColor="lightcoral" p={2}>
+      <Checkbox onChange={handleCheked} colorScheme="teal" isChecked={cheked}>
+        <Text as={todo.completed && "del"}>{todo.content}</Text>
+    
+      </Checkbox>
+      
+    </Box>
+    <Button variant="danger" onClick={()=>dispatch(Delete(todo.id))}>Delete
+    </Button>
+    </div>
+  )
+}
---
+++ src/redux/reducer/todos.js
@@ -0,0 +1,38 @@
+import { ADD_TODO, TOGGLE_TODO , SET_DELETE } from '../actionsTypes';
+
+
+const initialState = {
+  todos: []
+}
+
+const todos = (state = initialState, action) => {
+  switch(action.type) {
+    case ADD_TODO: {
+      const { id, content } = action.payload;
+      return {
+        todos: [
+          ...state.todos,
+          { content, completed: false, id }
+        ]
+      }
+    }
+
+    case TOGGLE_TODO: {
+      const { id } = action.payload;
+      const todos = state.todos.map(obj => obj.id === id ? { ...obj, completed: !obj.completed } : obj);
+      return { todos }
+    }
+    case SET_DELETE:{
+      
+      const todos = state.todos.filter(el=> el.id !==action.payload)
+      return {todos}
+     }
+    
+
+    default: {
+      return state;
+    }
+  }
+}
+
+export default todos;
